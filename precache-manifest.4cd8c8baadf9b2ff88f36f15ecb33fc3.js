cb317d5c007a4715fd122b762b49637f88cc33ec
mrrezaahmadi <mrrezaahmadi>
2020-07-16 15:41:58 +0430

    Updates

---
+++ precache-manifest.4cd8c8baadf9b2ff88f36f15ecb33fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb342b2d6f49596738a3f867b8d88ff",
+    "url": "/ReactJS-PhoneBook_CRUD/index.html"
+  },
+  {
+    "revision": "a482545fce15ad5837e3",
+    "url": "/ReactJS-PhoneBook_CRUD/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e1e7c028845f7c7df1e",
+    "url": "/ReactJS-PhoneBook_CRUD/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a482545fce15ad5837e3",
+    "url": "/ReactJS-PhoneBook_CRUD/static/js/2.602a4567.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactJS-PhoneBook_CRUD/static/js/2.602a4567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1e7c028845f7c7df1e",
+    "url": "/ReactJS-PhoneBook_CRUD/static/js/main.abeee6e0.chunk.js"
+  },
+  {
+    "revision": "cee0eb3cbbd8e03315f4",
+    "url": "/ReactJS-PhoneBook_CRUD/static/js/runtime-main.b6f4466a.js"
+  }
+]);
