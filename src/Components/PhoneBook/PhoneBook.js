aa1bd22f34191866cc6dcf1de38632005f85ce0b
mrrezaahmadi <mrrezaahmadi>
2020-07-18 23:19:40 +0430

    fix some minor issue

--- src/Components/PhoneBook/PhoneBook.js
+++ src/Components/PhoneBook/PhoneBook.js
@@ -56,6 +56,7 @@ function PhoneBook() {
     const { target: { name, value } } = event
     let editedContact = { ...prevContact, [name]: value }
 
+    console.log("hello world")
     copyContacts.find(contact => contact.id === prevContact.id).firstName = editedContact.firstName
     copyContacts.find(contact => contact.id === prevContact.id).lastName = editedContact.lastName
     copyContacts.find(contact => contact.id === prevContact.id).phone = editedContact.phone
